fd59f180bec3c82b4f2957ad5090716ff2b9602c
Matthijs Boet <m.boet@chello.nl>
2023-09-16 11:30:17 +0200

    called lenis in javascript code and added recommended css code

--- assets/js/scroll-animation.js
+++ assets/js/scroll-animation.js
@@ -51,3 +51,31 @@ gsap.from('.transition3', {
   duration: 1.2,
   stagger: .3
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const lenis = new Lenis();
+
+lenis.on('scroll', (e) => {
+	console.log(e);
+});
+
+function raf(time) {
+	lenis.raf(time);
+	requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);

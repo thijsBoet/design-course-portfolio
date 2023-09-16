1e4bf50223a777021cea7982b9d044f6b006148b
Matthijs Boet <m.boet@chello.nl>
2023-09-16 11:36:59 +0200

    removed lenis due to bugs in scrolling behaviour

--- assets/js/scroll-animation.js
+++ assets/js/scroll-animation.js
@@ -51,31 +51,3 @@ gsap.from('.transition3', {
   duration: 1.2,
   stagger: .3
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const lenis = new Lenis();
-
-lenis.on('scroll', (e) => {
-	console.log(e);
-});
-
-function raf(time) {
-	lenis.raf(time);
-	requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
